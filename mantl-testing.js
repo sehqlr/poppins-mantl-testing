0b94dcb49b14de99aa32213384417c18031b08c7
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-04-18 15:26:05 -0500

    Changed default configurations
    I replaced some of the sample config options for what they will be in
    the future

--- mantl-testing.js
+++ mantl-testing.js
@@ -1,6 +1,6 @@
 module.exports = function (poppins) {
 	poppins.plugins.exec.commands.push([
-			{ re: /^kickoff command here$/, exec: 'echo "Start mantl testing command"' }
+			{ re: /^@mantlbot start/, exec: 'echo "Start mantl testing command"' }
 	]);
 
 	poppins.plugins.mantl-testing.owners = ["sehqlr"];
--- poppins-config.js
+++ poppins-config.js
@@ -3,22 +3,22 @@ module.exports = function (poppins) {
     // Github repo to watch
     // https://github.com/myname/myrepo
     target: {
-      user: 'myname',
-      repo: 'myrepo'
+      user: 'sehqlr',
+      repo: 'https://github.com/sehqlr/mantl'
     },
 
     // Credentials for user who leaves comments, etc.
     // You may want to load these from a seperate file like `config-credentials.js`, and
     // add this file to your `.gitignore` list
     login: {
-      username: 'myrobotname',
-      password: 'supersecretpassword'
+      username: 'mantlbot',
+      password: ''
     },
 
     // port for poppins to listen on and URL for Github to ping
     hook: {
-      url: 'http://example.com:1234',
-      port: 1234
+      url: 'http://some-floating-ip:3643',
+      port: 3643
     }
   };
 
